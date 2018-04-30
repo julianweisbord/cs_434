c13f0694306983f1d2fcfbd6028b3ccb9e288926
julianweisbord <julianweisbord@gmail.com>
2018-04-29 22:33:46 -0700

    Adding KNN and Decision Tree
    Signed-off-by: julianweisbord <julianweisbord@gmail.com>

---
+++ knn/knn.py
@@ -0,0 +1,101 @@
+
+import math
+import operator
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def gen_k_values():
+    k = []
+    for i in range(1, 52, 2):
+        k.append(i)
+    return k
+
+def knn(train, test):
+    distances = []
+
+    ex_count = 0
+    # Iterate through each row not including the last element
+        # which is the label
+    # print("train: ", train)
+    # print("test: ", test)
+    # exit()
+    for row in train[:, 0:-1]:
+        # print("row: ", row)
+        # print("ex_count: ", ex_count)
+        # print("test[ex_count]", test[ex_count])
+        # print("test[ex_count][0:-1] ", test[ex_count][0:-1])
+        distance = euclidean_dist(test[0:-1], row)
+        distances.append((train[ex_count][-1], distance))
+        ex_count += 1
+
+    distances.sort(key=operator.itemgetter(1))
+    # print("distances", distances)
+    return distances
+
+def load_data():
+    train_data = np.genfromtxt("./knn_data/knn_train.csv", delimiter=",")
+    val_data = np.genfromtxt("./knn_data/knn_test.csv", delimiter=",")
+    X_train = train_data[:, 1:31]
+    Y_train = train_data[:, 0:1]
+    X_test = val_data[:, 1:31]
+    Y_test = val_data[:, 0:1]
+    return X_train, Y_train, X_test, Y_test
+
+
+def euclidean_dist(test_row, train_row):
+    distance = 0
+    num_features = 0
+    for feature in test_row:
+        # print("train_row[num_features]", train_row[num_features])
+        distance += pow((feature - train_row[num_features]), 2)
+        num_features += 1
+    return math.sqrt(distance)
+
+
+def accuracy(train, test, K, leave_out=False):
+    knn_alg = []
+    error = []
+
+    if leave_out:
+        knn_alg = knn(train, test)
+        error = []
+        for i, k in enumerate(K):
+            incorrect_num = 0
+            err = sum(pair[0] for pair in knn_alg[:k])
+            if err > 0 and test[-1] == -1 or err < 0 and test[-1] == 1:
+                incorrect_num += 1
+            error.append(incorrect_num)
+
+        return error
+
+    for example in test:
+        knn_alg.append(knn(train, example))
+
+    for k_val in K:
+        incorrect_count = np.float32(0.0)
+        row_num = 0
+        for row in knn_alg:
+            row_error = 0
+            for dist in row[:k_val]:
+                row_error += dist[0]
+            if(test[row_num][-1] == 1 and row_error < 0):
+                incorrect_count += 1
+            if(test[row_num][-1] == -1 and row_error > 0):
+                incorrect_count += 1
+            row_num += 1
+            # print("row_num",  row_num)
+        err = incorrect_count / len(train)
+        error.append(err)
+
+    return error
+
+
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ knn/main.py
@@ -0,0 +1,80 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import knn
+from dec_tree import DecTree
+
+PLOT = False
+D = [1, 2, 3, 4, 5, 6]
+
+def main():
+
+    X_train, Y_train, X_test, Y_test = knn.load_data()
+    print("X_train[0]", X_train[0])
+    print(X_test)
+    # Normalize, each feature will have a value between 0 and 1
+    new_dim = np.newaxis
+    X_train = X_train / X_train.sum(axis=1)[:, new_dim]
+    # count = 0
+    # for row in X_train:
+    #     for feature in row:
+    #         if float(feature) > 0.5:
+    #             count +=1
+    # print("Num train features greater than .5", count)
+    X_test = X_test / X_test.sum(axis=1)[:, new_dim]
+    print("X_train after normalize: ", X_train[0])
+    train_normal = np.append(X_train, Y_train, axis=1)
+    print("train[0]: ", train_normal[0])
+    test_normal = np.append(X_test, Y_test, axis=1)
+    print("test[0]: ", test_normal[0])
+    #
+    # # Implement the K-nearest neighbor algorithm, where K is a parameter.
+    #
+    K = knn.gen_k_values()
+    train_err = knn.accuracy(train_normal, train_normal, K)
+    print(train_err)
+
+    # Part 2 of Question 1:
+    train_error = knn.accuracy(train_normal, train_normal, K)
+
+    temp = [[] for _ in range(len(test_normal))]
+    cross_error = []
+
+    for i, example in enumerate(train_normal):
+
+        train_normal = np.delete(train_normal, i, 0)
+        temp[i] = knn.accuracy(train_normal, example, K, leave_out=True)
+        train_normal = np.insert(train_normal, i, example, 0)
+
+    for total in np.sum(temp, axis=0):
+        cross_error.append(np.float32(total) / (len(test_normal) -1))
+
+    print("!!!!!!Cross results", cross_error)
+
+    test_error = knn.accuracy(train_normal, test_normal, K)
+
+    print("!!!!!!training results", train_error)
+    print("!!!!!!testing results", test_error)
+    print(len(train_error), len(test_error))
+
+
+    if PLOT:
+        plt.plot(K, train_error)
+        plt.plot(K, test_error)
+        print("len cross: ", len(cross_error))
+        plt.plot(K, cross_error)
+        plt.xlabel('K values')
+        plt.ylabel('Error')
+        plt.show()
+
+    # Question 2, Decision Trees:
+    # Create Decision Stump:
+    # train = np.genfromtxt("knn_data/knn_train.csv", delimiter=",")
+    # print("new train", train)
+    stump = DecTree(train_normal, test_normal, 1, depth_limit=1, stump=True)
+    for d in D:
+        tree = DecTree(train_normal, test_normal, 10, d)
+
+
+if __name__ == "__main__":
+    main()
